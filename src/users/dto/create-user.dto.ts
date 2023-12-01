0cc442d5c4f26feb413811c2caa7343f906b1cc0
AlexGitCode <lineale@hotmail.com>
2023-12-01 15:10:11 +0100

    feat : Validation pipe installation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
-export class CreateUserDto {
-    pseudo: string;
-    mail: string
+import { IsEmail, IsString, Length } from 'class-validator';
 
+export class CreateUserDto {
+  @IsString()
+  @Length(5, 50)
+  pseudo: string;
+
+  @IsEmail()
+  @Length(4, 75)
+  mail: string;
 }
